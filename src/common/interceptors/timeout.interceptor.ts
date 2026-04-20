2142040866fc0c53939640e0c365d656dd30a08f
Carlos Aguirre <carlos.aguirre@it-seekers.com>
2026-04-20 17:06:03 -0600

    microservices

---
+++ src/common/interceptors/timeout.interceptor.ts
@@ -0,0 +1,16 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  NestInterceptor,
+} from "@nestjs/common";
+import { Observable } from "rxjs";
+import { timeout } from "rxjs/operators";
+
+export class TimeOutInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>
+  ): Observable<any> {
+    return next.handle().pipe(timeout(120000));
+  }
+}
---
+++ src/interfaces/flight.interface.ts
@@ -0,0 +1,10 @@
+import { IPassenger } from "./passenger.interface";
+
+export interface IFlight  {
+   
+     pilot:string;
+     airplane: string;      
+     destinationCity: string;    
+     flightDate: Date;    
+     passengers:IPassenger[];
+}
